b817fa495e7ae697b96c3be905fcd942c76cdd57
Stein Elgethun <ston3nots@gmail.com>
2020-12-01 15:24:43 +0100

    Use same predicate for both parts

--- 2020/1/1.pl
+++ 2020/1/1.pl
@@ -4,13 +4,15 @@
 integers([]) --> call(eos), !.
 integers([X|Xs]) --> integer(X), "\n", integers(Xs).
 
-sum2(List, P) :-
+%% part 1
+solve(List, P) :-
     select(X, List, L1),
     select(Y, L1, _),
     2020 is X + Y,
     P is X * Y.
 
-sum3(List, P) :-
+%% part 2
+solve(List, P) :-
     select(X, List, L1),
     select(Y, L1, L2),
     select(Z, L2, _),
