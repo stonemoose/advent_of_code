1cd96cfe7ec8ec5b8661c42f92f33f58d07c6d40
Stein Elgethun <ston3nots@gmail.com>
2020-12-02 10:44:13 +0100

    Make solution prettier

--- 2020/02/2.py
+++ 2020/02/2.py
@@ -1,22 +1,19 @@
+import re
+
+
 def valid_password(line):
-    rule, password = line.strip().split(': ')
-    min_l, max_l = rule.split('-')
-    max_l, letter = max_l[:-2], max_l[-1]
-    min_l = int(min_l)
-    max_l = int(max_l)
+    min_l, max_l, letter, password = re.split(r'-| |: ', line.strip())
+    min_l, max_l = int(min_l), int(max_l)
     return min_l <= password.count(letter) <= max_l
 
 
-print(sum([valid_password(line) for line in open('input').readlines()]))
-
-
 def valid_password_part_two(line):
-    rule, password = line.strip().split(': ')
-    first, second = rule.split('-')
-    second, letter = second[:-2], second[-1]
-    first = int(first)-1
-    second = int(second)-1
+    first, second, letter, password = re.split(r'-| |: ', line.strip())
+    first, second = int(first), int(second)
     return (password[first] == letter) ^ (password[second] == letter)
 
 
+print(sum([valid_password(line) for line in open('input').readlines()]))
 print(sum([valid_password_part_two(line) for line in open('input').readlines()]))
+
+
