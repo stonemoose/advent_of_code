c0a788a727f0c77e686092e6fe19da2ef001b138
Stein Elgethun <ston3nots@gmail.com>
2020-12-02 09:49:05 +0100

    Add oneliner

---
+++ 2020/day_2/oneline.py
@@ -0,0 +1,5 @@
+import re
+
+print(sum(map(lambda x: int(x[0]) <= x[3].count(x[2]) <= int(x[1]), (re.split(r'-| |: ', l) for l in open('input').readlines()))))
+
+print(sum(map(lambda x: (x[3][int(x[0])-1] == x[2]) ^ (x[3][int(x[1])-1] == x[2]), (re.split(r'-| |: ', l) for l in open('input').readlines()))))
