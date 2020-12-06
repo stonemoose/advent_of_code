9596a84cdde7c9619d28b82890f8e320f90401d0
Stein Elgethun <ston3nots@gmail.com>
2020-12-06 11:44:28 +0100

    Shorten day 5 oneliners

--- 2020/05/oneliner.py
+++ 2020/05/oneliner.py
@@ -1,4 +1,4 @@
 # Part 1
-print(int(max(open('input').read().replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0').splitlines()), base=2))
+print(max([int(''.join(map(lambda x: ('1', '0')['B' < x < 'R'], l)), 2) for l in open('input').read().splitlines()]))
 # Part 2
-(lambda x: print(set(range(x[0], x[-1])).difference(x).pop()))(sorted([int(i, base=2) for i in open('input').read().replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0').splitlines()]))
+(lambda x: print(set(range(x[0], x[-1])).difference(x).pop()))(sorted([int(''.join(map(lambda x: ('1', '0')['B' < x < 'R'], l)), 2) for l in open('input').read().splitlines()]))
