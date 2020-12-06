84d17026ff0cdff22e1b31cbab6facf22953b1f2
Stein Elgethun <ston3nots@gmail.com>
2020-12-06 11:18:42 +0100

    Add oneliner day 6

---
+++ 2020/06/oneliner.py
@@ -0,0 +1,2 @@
+print(sum([len(set(l.replace('\n', ''))) for l in open('input').read().strip().split('\n\n')]))
+print(sum([len(set.intersection(*map(set, l.split('\n')))) for l in open('input').read().strip().split('\n\n')]))
