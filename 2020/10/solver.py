8bd85cf5c071e4d50b9df2ce66e7937e3e0a56dd
Stein Elgethun <ston3nots@gmail.com>
2020-12-10 10:39:57 +0100

    Use int instead of lambda 0

--- 2020/10/solver.py
+++ 2020/10/solver.py
@@ -5,7 +5,7 @@ with open('input') as f:
     numbers = [0] + sorted([int(n) for n in f.read().strip().split('\n')])
 numbers.append(numbers[-1]+3)
 
-possible = defaultdict(lambda: 0, {num: 0 for num in numbers})
+possible = defaultdict(int, {num: 0 for num in numbers})
 possible[numbers[-1]] = 1
 for i in range(len(numbers)-1, 0, -1):
     possible[numbers[i]-3] += possible[numbers[i]]
