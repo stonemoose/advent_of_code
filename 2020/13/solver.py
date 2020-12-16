35eab8b0a921754aaf63c4bac73b63cada6fc64e
Stein Elgethun <ston3nots@gmail.com>
2020-12-16 16:51:32 +0100

    Shorten day 13 solution

--- 2020/13/solver.py
+++ 2020/13/solver.py
@@ -4,15 +4,12 @@ with open('input') as f:
 buses = enumerate(buses.split(','))
 buses = [(i % int(bus), int(bus)) for i, bus in buses if not bus == 'x']
 start_time = int(start_time)
-t_delta = buses[0][1]
-t = 0
+t_delta = t = buses[0][1]
 
 for i, bus in buses[1:]:
-    while True:
+    while bus - (t % bus) != i:
         t += t_delta
-        if bus - (t % bus) == i:
-            t_delta *= bus
-            break
+    t_delta *= bus
 
 first = min(buses, key=lambda x: x[1] - start_time % x[1])[1]
 print(f'Part 1: {first * (first - (start_time % first))}')
