968d4a25ed7a7b4e31fabfc87e156c050b60ef35
Stein Elgethun <ston3nots@gmail.com>
2021-12-06 07:03:38 +0100

    Update day 6 solver

--- 2021/06/solver.py
+++ 2021/06/solver.py
@@ -1,18 +1,16 @@
 
-def fish_by_day(day, timer_dict):
+def fish_by_day(day, fish_timers):
     for _ in range(day):
-        next_timer_dict = {i: timer_dict[i+1] for i in range(8)}
-        next_timer_dict[8] = timer_dict[0]
-        next_timer_dict[6] += timer_dict[0]
-        timer_dict = next_timer_dict
-    return timer_dict
+        fish_timers = [fish_timers[(i+1) % 9] for i in range(9)]
+        fish_timers[6] += fish_timers[8]
+    return fish_timers
 
 
 if __name__ == '__main__':
     with open('input') as f:
-        timer_dict = {i: 0 for i in range(9)}
+        fish_timers = [0]*9
         for num in f.read().strip().split(','):
-            timer_dict[int(num)] += 1
+            fish_timers[int(num)] += 1
 
-    print(f'Part 1: {sum(fish_by_day(80, timer_dict).values())}')
-    print(f'Part 2: {sum(fish_by_day(256, timer_dict).values())}')
+    print(f'Part 1: {sum(fish_by_day(80, fish_timers))}')
+    print(f'Part 2: {sum(fish_by_day(256, fish_timers))}')
