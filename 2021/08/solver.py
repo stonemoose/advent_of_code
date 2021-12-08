ff40e590d88a76835d119a491d37e814369260e3
Stein Elgethun <stein.elgethun@usit.uio.no>
2021-12-08 11:07:46 +0100

    Clean up day 8

--- 2021/08/solver.py
+++ 2021/08/solver.py
@@ -1,83 +1,45 @@
 with open('input') as f:
     lines = [line.strip().split(' | ') for line in f.readlines()]
+    lines = [([set(p) for p in segment_patterns.split()],
+              [set(n) for n in output.split()])
+             for segment_patterns, output in lines]
 
 
-ans = 0
-final_ans2 = 0
-for pattern, output in lines:
-    segments = {
-        'top_left': '',
-        'bot_left': '',
-        'top': '',
-        'mid': '',
-        'bot': '',
-        'top_right': '',
-        'bot_right': '',
-    }
-    patterns = pattern.split(' ')
-    zero = None
-    one = None
-    two = None
-    three = None
-    four = None
-    five = None
-    six = None
-    seven = None
-    eight = None
-    nine = None
-    for p in list(patterns):
-        if len(p) == 2:
-            one = p
-            patterns.remove(p)
-        if len(p) == 3:
-            seven = p
-            patterns.remove(p)
-        if len(p) == 4:
-            four = p
-            patterns.remove(p)
-        if len(p) == 7:
-            eight = p
-            patterns.remove(p)
-    for p in patterns:
-        if len(set(p).intersection(four)) == 4:
-            nine = p
-        elif len(p) == 6:
-            if len(set(p).intersection(one)) == 2:
-                zero = p
+part1 = 0
+part2 = 0
+for patterns, output in lines:
+    numbers = [None] * 10
+    for pattern in list(patterns):
+        num = 0
+        if len(pattern) == 2:
+            num = 1
+        elif len(pattern) == 4:
+            num = 4
+        elif len(pattern) == 3:
+            num = 7
+        elif len(pattern) == 7:
+            num = 8
+        if num:
+            numbers[num] = pattern
+            patterns.remove(pattern)
+    for pattern in patterns:
+        if len(pattern) == 6:
+            if numbers[4].issubset(pattern):
+                num = 9
+            elif numbers[1].issubset(pattern):
+                num = 0
             else:
-                six = p
-        elif len(set(p).intersection(one)) == 2:
-            three = p
-        elif len(set(p).intersection(four)) == 3:
-            five = p
+                num = 6
+        elif numbers[1].issubset(pattern):
+            num = 3
+        elif len(pattern.intersection(numbers[4])) == 3:
+            num = 5
         else:
-            two = p
-    ans2 = 0
-    for num in output.split(' '):
-        ans2 *= 10
-        if len(num) in (2, 3, 4, 7):
-            ans += 1
-        if set(num) == set(zero):
-            pass
-        elif set(num) == set(one):
-            ans2 += 1
-        elif set(num) == set(two):
-            ans2 += 2
-        elif set(num) == set(three):
-            ans2 += 3
-        elif set(num) == set(four):
-            ans2 += 4
-        elif set(num) == set(five):
-            ans2 += 5
-        elif set(num) == set(six):
-            ans2 += 6
-        elif set(num) == set(seven):
-            ans2 += 7
-        elif set(num) == set(eight):
-            ans2 += 8
-        elif set(num) == set(nine):
-            ans2 += 9
-    final_ans2 += ans2
-    print(ans2)
-print(ans)
-print(final_ans2)
+            num = 2
+        numbers[num] = pattern
+
+    part2 += int(''.join([str(numbers.index(num)) for num in output]))
+    part1 += len([num for num in output if numbers.index(num) in (1, 4, 7, 8)])
+
+print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')
