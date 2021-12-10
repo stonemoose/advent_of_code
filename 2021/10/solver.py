7579fde05ba4b8c74443184c1bbc051579f4d713
Stein Elgethun <ston3nots@gmail.com>
2021-12-10 23:32:25 +0100

    Clean up python solution day 10

--- 2021/10/solver.py
+++ 2021/10/solver.py
@@ -3,46 +3,27 @@ with open('input') as f:
     lines = [line.strip() for line in f.readlines()]
 
 
-matching = {
-    '(': ')',
-    '[': ']',
-    '{': '}',
-    '<': '>'
-}
+matching = dict(['()', '[]', '{}', '<>'])
+corrupt_points = {')': 3, ']': 57, '}': 1197, '>': 25137}
 
-corrupted = 0
-total_out = []
+illegal_chars = []
+incomplete_points = []
 for line in lines:
-    line_out = 0
     opened = []
     for char in line:
         if char in '([{<':
             opened.append(char)
-            continue
-        elif not opened:
-            break
-        elif char == ')' and opened.pop() != '(':
-            corrupted += 3
-            opened = None
-            break
-        elif char == ']' and opened.pop() != '[':
-            corrupted += 57
-            opened = None
-            break
-        elif char == '}' and opened.pop() != '{':
-            corrupted += 1197
-            opened = None
-            break
-        elif char == '>' and opened.pop() != '<':
-            corrupted += 25137
-            opened = None
-            break
-    if opened:
+        else:
+            if char != matching[opened.pop()]:
+                illegal_chars.append(char)
+                break
+    else:
+        line_out = 0
         for char in opened[::-1]:
             line_out *= 5
             line_out += '0([{<'.index(char)
-        total_out.append(line_out)
+        incomplete_points.append(line_out)
 
-total_out.sort()
-print(corrupted)
-print(total_out[len(total_out)//2])
+incomplete_points.sort()
+print('Part 1:', sum([corrupt_points[char] for char in illegal_chars]))
+print('Part 2:', incomplete_points[len(incomplete_points)//2])
