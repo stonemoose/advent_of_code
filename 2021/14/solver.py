3721465997edd4ff648554ee0d2db83468f2e9e0
Stein Elgethun <ston3nots@gmail.com>
2021-12-14 12:23:40 +0100

    Clean up day 14

--- 2021/14/solver.py
+++ 2021/14/solver.py
@@ -1,14 +1,14 @@
 from collections import Counter
 
 
-def next_step(formula_counter):
+def next_step(formula_counter, pair_rules):
     new_formula = Counter()
-    for p in formula_counter:
-        if pairs[p]:
-            new_formula[p[0]+pairs[p]] += formula_counter[p]
-            new_formula[pairs[p] + p[1]] += formula_counter[p]
-        else:
-            new_formula[p] += formula_counter[p]
+    for key in formula_counter:
+        try:
+            new_formula[key[0] + pair_rules[key]] += formula_counter[key]
+            new_formula[pair_rules[key] + key[1]] += formula_counter[key]
+        except KeyError:
+            new_formula[key] += formula_counter[key]
     return new_formula
 
 
@@ -30,13 +30,13 @@ if __name__ == '__main__':
 
     with open('input') as f:
         formula, pairs = f.read().strip().split('\n\n')
-    pairs = dict([p.split(' -> ') for p in pairs.split('\n')])
+    pair_rules = dict([p.split(' -> ') for p in pairs.split('\n')])
     formula_counter = Counter(a+b for a, b in zip(formula[:-1], formula[1:]))
 
     for _ in range(10):
-        formula_counter = next_step(formula_counter)
+        formula_counter = next_step(formula_counter, pair_rules)
     print('Part 1: ', most_least_difference(formula_counter, formula))
 
     for _ in range(10, 40):
-        formula_counter = next_step(formula_counter)
+        formula_counter = next_step(formula_counter, pair_rules)
     print('Part 2: ', most_least_difference(formula_counter, formula))
