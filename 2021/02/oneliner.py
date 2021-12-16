7fb19603122431915300fb69f5cc7a299d177683
Stein Elgethun <ston3nots@gmail.com>
2021-12-16 21:00:03 +0100

    Add oneline day 2 part 1

---
+++ 2021/02/oneliner.py
@@ -0,0 +1,1 @@
+print((lambda l: l[0]*l[1])([sum(i) for i in zip(*map(lambda t: (int(t[1])*(t[0][0]=='f'),int(t[1])*(t[0][0]=='d')-int(t[1])*(t[0][0]=='u')),[l.strip().split() for l in open('input').readlines()]))]))
