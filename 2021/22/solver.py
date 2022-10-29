7f0898ba29e0bf7768be660e3cb1e647b8fe2a64
Stein Elgethun <ston3nots@gmail.com>
2022-10-29 11:50:10 +0200

    Add part 1 day 22 solver

---
+++ 2021/22/solver.py
@@ -0,0 +1,20 @@
+
+cubes = set()
+
+with open('input') as f:
+    for line_num, line in enumerate(f):
+        command, coords = line.strip().split()
+        x, y, z = ([int(i) for i in c[2:].split('..')] for c in coords.split(','))
+        print(command, x, y, z)
+        for x_coord in range(x[0], x[1]+1):
+            for y_coord in range(y[0], y[1]+1):
+                for z_coord in range(z[0], z[1]+1):
+                    if command == 'on':
+                        cubes.add((x_coord, y_coord, z_coord))
+                    else:
+                        cubes.discard((x_coord, y_coord, z_coord))
+        print(line_num)
+        if line_num == 19:
+            break
+
+print(len(cubes))
