35090d7dd1e4f320c9011ad50249d2960f18de74
Stein Elgethun <ston3nots@gmail.com>
2022-12-04 14:48:20 +0100

    Remove explicit cast to list in oneliners

--- 2022/04/oneliners.py
+++ 2022/04/oneliners.py
@@ -1,3 +1,3 @@
 # fmt: off
-print(sum((lambda a,b,c,d:(a<=c)&(b>=d)|(a>=c)&(b<=d))(*list(map(int,l.strip().replace("-",",").split(",")))) for l in open("input")))
-print(sum((lambda a,b,c,d:max(a,c)<=min(b,d))(*list(map(int,l.strip().replace("-",",").split(",")))) for l in open("input")))
+print(sum((lambda a,b,c,d:(a<=c)&(b>=d)|(a>=c)&(b<=d))(*(map(int,l.strip().replace("-",",").split(",")))) for l in open("input")))
+print(sum((lambda a,b,c,d:max(a,c)<=min(b,d))(*(map(int,l.strip().replace("-",",").split(",")))) for l in open("input")))
