2b567e2219ec40bffc2e305fcf6c415166a3d0a7
Stein Elgethun <ston3nots@gmail.com>
2022-12-05 07:40:44 +0100

    Golf a bit more on day 3 oneliners

--- 2022/03/oneline.py
+++ 2022/03/oneline.py
@@ -1,3 +1,3 @@
 # fmt: off
-print(sum("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".index((set(x[len(x)//2:])&set(x[:len(x)//2])).pop()) for x in open('2022/03/input').readlines()))
-print(sum("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".index((set(x)&set(y)&set(z)).pop()) for x,y,z in zip(*[iter(open('2022/03/input').read().split('\n'))]*3)))
+print(sum(__import__('string').ascii_letters.index((set(x[len(x)//2:])&set(x[:len(x)//2])).pop())+1 for x in open('2022/03/input').readlines()))
+print(sum(__import__('string').ascii_letters.index((set(x)&set(y)&set(z)).pop())+1 for x,y,z in zip(*[iter(open('2022/03/input').read().split('\n'))]*3)))
