27070b2d176f2d833cc16755d9767294592899be
Stein Elgethun <ston3nots@gmail.com>
2022-12-07 09:54:21 +0100

    Clean up day 7

--- 2022/07/solver.py
+++ 2022/07/solver.py
@@ -1,42 +1,24 @@
-with open("input") as f:
-    input = f.read().strip().split("\n")
+from aocd.models import Puzzle
 
+puzzle = Puzzle(year=2022, day=7)
+input = puzzle.input_data.split("\n")
 
-system = {"/": 0}
-current_dir = ["/"]
-counted = set()
-for line in input[1:]:
-    line = line.strip()
-    if line[2:4] == "cd":
-        if line[5:7] == "..":
+system = {}
+current_dir = []
+for line in input:
+    match line.strip().split():
+        case "$", "cd", "..":
             current_dir.pop()
-        else:
-            current_dir.append(line[5:])
+        case "$", "cd", dir_name:
+            current_dir.append(dir_name)
             system["/".join(current_dir)] = 0
-    elif line[2:4] == "ls":
-        pass
-    elif line.split()[0] == "dir":
-        pass
-    else:
-        num, name = line.split()
-        if not ("/".join(current_dir) + name) in counted:
+        case size, _ if size.isdigit():
             m = "/"
-            system[m] += int(num)
+            system[m] += int(size)
             for x in current_dir[1:]:
                 m += "/" + x
-                system[m] += int(num)
+                system[m] += int(size)
 
-            counted.add("/".join(current_dir) + name)
-
-part1 = 0
-for key, value in system.items():
-    if value <= 100000:
-        part1 += value
-print(f"Part 1: {part1}")
-
+puzzle.answer_a = sum(size for size in system.values() if size <= 100000)
 free_space = 70000000 - system["/"]
-possible = []
-for key, value in system.items():
-    if free_space + value >= 30000000:
-        possible.append(value)
-print(f"Part 2: {min(possible)}")
+puzzle.answer_b = min(size for size in system.values() if size + free_space >= 30000000)
