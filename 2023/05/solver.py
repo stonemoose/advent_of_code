90a8ad6f5b2ad9fa5459adb2c0d960df0af8a23c
Stein Elgethun <ston3nots@gmail.com>
2023-12-05 07:18:00 +0100

    Add day 5 solver

---
+++ 2023/05/solver.py
@@ -0,0 +1,55 @@
+def map_next_category(category, maps):
+    next_category = []
+    for item in category:
+        for min_max, increase in maps.items():
+            if item >= min_max[0] and item <= min_max[1]:
+                next_category.append(item + increase)
+                break
+        else:
+            next_category.append(item)
+    return next_category
+
+
+def map_next_category_range(category, maps):
+    next_category = []
+    while category:
+        start, end = category.pop(0)
+        for min_max, increase in maps.items():
+            if start >= min_max[0] and start <= min_max[1]:
+
+                if end <= min_max[1]:
+                    next_category.append([start + increase, end + increase])
+                    break
+
+                else:
+                    next_category.append([start + increase, min_max[1] + increase])
+                    category.append([min_max[1] + 1, end])
+                    break
+
+            elif end <= min_max[1] and end >= min_max[0]:
+                next_category.append([min_max[0] + increase, end + increase])
+                category.append([start, min_max[0] - 1])
+                break
+        else:
+            next_category.append([start, end])
+    return next_category
+
+
+if __name__ == "__main__":
+    with open("input") as f:
+        text = f.read().strip().split("\n\n")
+
+    seeds = [int(t) for t in text[0].split(":")[1].split()]
+    range_seeds = [
+        [seeds[i], sum(seeds[i : i + 2])] for i in range(0, len(seeds) // 2, 2)
+    ]
+
+    for line in text[1:]:
+        maps = {}
+        for mapping in line.split("\n")[1:]:
+            mapping = [int(t) for t in mapping.split()]
+            maps[(mapping[1], mapping[1] + mapping[2] - 1)] = mapping[0] - mapping[1]
+        seeds = map_next_category(seeds, maps)
+        range_seeds = map_next_category_range(range_seeds, maps)
+    print("Part 1: ", min(seeds))
+    print("Part 2: ", min(range_seeds)[0])
