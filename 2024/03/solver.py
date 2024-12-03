503efd41ddb5b13944eed7416a536dc1dd390140
Stein Elgethun <stein.elgethun@gmail.com>
2024-12-03 15:03:06 +0100

    Add day3 p2 oneliner

--- 2024/03/solver.py
+++ 2024/03/solver.py
@@ -7,12 +7,12 @@ def solve(input_data, part2=False):
     if part2:
         end_disabled = re.search(r"(?s:don't\(\)(?:(?!do\(\)).)*$)", input_data)
         if end_disabled:
-            input_data = input_data[:end_disabled.start()]
+            input_data = input_data[: end_disabled.start()]
         regex = r"(?s:don't\(\).*?do\(\).*?)?" + regex
     return sum((int(n1) * int(n2) for n1, n2 in re.findall(regex, input_data)))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     puzzle = Puzzle(2024, 3)
 
     ex = puzzle.examples[0]
@@ -23,4 +23,3 @@ if __name__=="__main__":
 
     assert solve(ex2, part2=True) == 48
     puzzle.answer_b = solve(puzzle.input_data, part2=True)
-
