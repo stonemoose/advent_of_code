8b23e9772ec87dc8001a2f1f22ed5e29d63e44f5
Stein Elgethun <stein.elgethun@gmail.com>
2024-12-03 07:22:59 +0100

    Go back to old solution since regex was bugged

--- 2024/03/solver.py
+++ 2024/03/solver.py
@@ -2,21 +2,51 @@ import re
 from aocd.models import Puzzle
 
 
-def solve(input_data, part2=False):
-    regex = r"mul\((\d+),(\d+)\)"
-    if part2:
-        regex = r"(?s:don't\(\).*?do\(\).*)?" + regex
+def parse(input_data, part2=False):
+    out = []
+    enabled = True
+    for line in input_data.strip().split("\n"):
+        if part2:
+            line_data = []
+            for i in range(len(line)):
+                if enabled and (m := re.match(r"^mul\(\d+,\d+\)", line[i:])):
+                    line_data.append(m.group(0))
+                if not enabled and re.match(r"^do\(\)", line[i:]):
+                    enabled = True
+                if enabled and re.match(r"^don't\(\)", line[i:]):
+                    enabled = False
 
-    return sum((int(n1) * int(n2) for n1, n2 in re.findall(regex, input_data)))
+            out.append(line_data)
+        else:
+            out.append(re.findall(r"mul\(\d+,\d+\)", line))
+    return out
+
+def helper(line):
+    out = 0
+    for thing in line:
+        nums = re.findall(r"\d+", thing)
+        assert len(nums) == 2
+        out += int(nums[0])*int(nums[1])
+    return out
+
+def solve(parsed):
+    ans = 0
+    for line in parsed:
+        ans += helper(line)
+    return ans
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     puzzle = Puzzle(2024, 3)
 
+    parsed = parse(puzzle.input_data)
     ex = puzzle.examples[0]
-    if solve(ex.input_data) == ex.answer_a:
-        puzzle.answer_a = solve(puzzle.input_data)
+    assert solve(parse(ex.input_data)) == 161
+    puzzle.answer_a = solve(parsed)
 
     ex2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-    if solve(ex2, True) == 48:
-        parsed = solve(puzzle.input_data, True)
+
+    assert solve(parse(ex2, part2=True)) == 48
+    parsed = parse(puzzle.input_data, part2=True)
+    puzzle.answer_b = solve(parsed)
+
