de591d88ff805d7efd63e1f6a2608ba9ad082051
Stein Elgethun <stein.elgethun@gmail.com>
2024-12-04 09:42:34 +0100

    Update day 3 oneliner

--- 2024/03/oneliner.py
+++ 2024/03/oneliner.py
@@ -2,5 +2,6 @@
 import re
 from aocd.models import Puzzle
     
-print(sum((int(n1) * int(n2) for n1, n2 in re.findall(r"mul\((\d+),(\d+)\)", Puzzle(2024, 3).input_data))))
-print(sum((int(n1) * int(n2) for n1, n2 in re.findall(r"(?s:don't\(\).*?do\(\).*?)?mul\((\d+),(\d+)\)|(?s:don't\(\)(?:(?!do\(\)).)*$)", Puzzle(2024, 3).input_data) if n1)))
+print(sum((int(a) * int(b) for a, b in re.findall(r"mul\((\d+),(\d+)\)", Puzzle(2024, 3).input_data))))
+print(sum((int(a) * int(b) for a, b in re.findall(r"mul\((\d+),(\d+)\)", re.sub(r"don't\(\)(?s:(?!do\(\)).)*", "", Puzzle(2024, 3).input_data)))))
+
