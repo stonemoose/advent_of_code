78b812dc03b4a6761d4986cb4974c47521e2e338
Stein Elgethun <stein.elgethun@gmail.com>
2025-12-09 21:33:51 +0100

    Optimize day 9 solution

--- 2025/09/solver.py
+++ 2025/09/solver.py
@@ -12,15 +12,20 @@ def rectangle_size(start, end):
     return (abs(start[0] - end[0]) + 1) * (abs(start[1] - end[1]) + 1)
 
 
-def inside_line(line, start, end):
+def crosses_line(line, start, end):
+    square = [start, (start[0], end[1]), end, (end[0], start[1])]
+    edges = list(zip(square, square[1:] + [square[0]]))
+
     if line[0][0] == line[1][0]:
         return any(
-            inside((line[0][0], point), start, end)
-            for point in range(min(line[0][1], line[1][1]), max(line[0][1], line[1][1]))
+            min(line[0][1], line[1][1]) <= edge[0][1] <= max(line[0][1], line[1][1])
+            and min(edge[0][0], edge[1][0]) < line[0][0] < max(edge[0][0], edge[1][0])
+            for edge in edges
         )
     return any(
-        inside((point, line[0][1]), start, end)
-        for point in range(min(line[0][0], line[1][0]), max(line[0][0], line[1][0]))
+        min(line[0][0], line[1][0]) <= edge[0][0] <= max(line[0][0], line[1][0])
+        and min(edge[0][1], edge[1][1]) < line[0][1] < max(edge[0][1], edge[1][1])
+        for edge in edges
     )
 
 
@@ -35,18 +40,19 @@ def inside(point, start, end):
 
 def solve(input_data):
     p1 = p2 = 0
-    parsed = parse(input_data)
-    lines = [(start, end) for start, end in zip(parsed, parsed[1:] + [parsed[0]])]
+    points = parse(input_data)
+    lines = [(start, end) for start, end in zip(points, points[1:] + [points[0]])]
 
     all_rectangles = []
-    for i, start in enumerate(parsed, 1):
-        for end in parsed[i:]:
+    # O(n^2), n = punkter for å lage rektangler
+    for i, start in enumerate(points, 1):
+        for end in points[i:]:
             all_rectangles.append((rectangle_size(start, end), start, end))
     all_rectangles.sort(reverse=True)
     p1 = all_rectangles[0][0]
 
     for size, start, end in all_rectangles:
-        if not any(inside_line(line, start, end) for line in lines):
+        if not any(crosses_line(line, start, end) for line in lines):
             p2 = size
             break
 
